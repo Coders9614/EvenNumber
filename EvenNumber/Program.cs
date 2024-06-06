09e3fb5529b2ddc98d668826f03b0236fefba708
Coders9614 <joonyong.kim1@outlook.com>
2024-06-06 09:04:57 -0600

    Add project files.

---
+++ EvenNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class Program
+    {
+        //count even number to recur method. 
+        public static string ReturnEvenNumbers(int num)
+        {
+            if (num >= 100)
+            {
+                return ""; // Base case: Stop when num reaches 100
+            }
+
+            // Recur for the next even number
+            return num + " " + ReturnEvenNumbers(num + 2);
+        }
+
+    public static void Main(string[] args)
+    {
+        string evenNumbers = ReturnEvenNumbers(2);
+        Console.WriteLine(evenNumbers);
+        Console.ReadKey();
+    }
+
+}
+
